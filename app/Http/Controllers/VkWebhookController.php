a88f19f0b52268cc7d3061173e39b9e5f29c514b
vla5924 <vla5924@gmail.com>
2021-05-28 14:14:38 +0300

    vk webhook debugged

--- app/Http/Controllers/VkWebhookController.php
+++ app/Http/Controllers/VkWebhookController.php
@@ -36,30 +36,30 @@ class VkWebhookController extends Controller
         if ($request->type == 'wall_post_new') {
             $postData = $request->object;
 
-            if ($postData->marked_as_ads || $postData->post_type != 'post')
+            if ($postData['marked_as_ads'] || $postData['post_type'] != 'post')
                 return self::SUCCESS_TEXT;
 
             $post = new Post;
 
-            $group = Group::where('vk_id', -$postData->owner_id)->first();
+            $group = Group::where('vk_id', -$postData['owner_id'])->first();
             if (!$group)
                 return self::FALLBACK_TEXT;
             $post->group_id = $group->id;
 
-            $creator = User::where('vk_id', $postData->created_by)->first();
-            $signer = User::where('vk_id', $postData->signer_id)->first();
-            $post->created_by = $creator ? $creator->id : null;
-            $post->signed_by = $signer ? $signer->id : null;
+            $creator = User::where('vk_id', $postData['created_by'])->first();
+            $signer = isset($postData['signer_id']) ? User::where('vk_id', $postData['signer_id'])->first() : null;
+            $post->creator_id = $creator ? $creator->id : null;
+            $post->signer_id = $signer ? $signer->id : null;
 
-            $post->text = self::cleanUpText($postData->text);
-            $post->vk_id = $postData->id;
+            $post->text = self::cleanUpText($postData['text']);
+            $post->vk_id = $postData['id'];
             $post->save();
 
-            if (!$postData->attachments)
+            if (!isset($postData['attachments']))
                 return self::SUCCESS_TEXT;
 
-            foreach ($postData->attachments as $attachmentData) {
-                $type = $attachmentData->type;
+            foreach ($postData['attachments'] as $attachmentData) {
+                $type = $attachmentData['type'];
                 if (!\in_array($type, self::ATTACHMENT_TYPES))
                     continue;
 
@@ -67,12 +67,12 @@ class VkWebhookController extends Controller
                 $attachment->post_id = $post->id;
 
                 if ($type == 'photo') {
-                    if ($attachmentData->photo->owner_id != $postData->owner_id)
+                    if ($attachmentData['photo']['owner_id'] != $postData['owner_id'])
                         continue;
 
                     $sizes = [];
-                    foreach ($attachmentData->photo->sizes as $size)
-                        $sizes[$size->type] = $size->url;
+                    foreach ($attachmentData['photo']['sizes'] as $size)
+                        $sizes[$size['type']] = $size['url'];
                     if (isset($sizes["w"])) $sbig = "w";
                     elseif (isset($sizes["z"])) $sbig = "z";
                     elseif (isset($sizes["y"])) $sbig = "y";
@@ -84,7 +84,7 @@ class VkWebhookController extends Controller
                     elseif (isset($sizes["s"])) $ssmall = "s";
 
                     $attachment->type = 'photo';
-                    $attachment->vk_id = $attachmentData->photo->id;
+                    $attachment->vk_id = $attachmentData['photo']['id'];
                     $attachment->meta = [
                         'lg' => $sizes[$sbig],
                         'sm' => $sizes[$ssmall],
@@ -94,14 +94,16 @@ class VkWebhookController extends Controller
                 }
 
                 if ($type == 'video') {
-                    if ($attachmentData->video->owner_id != $postData->owner_id)
-                        continue;
-
                     $attachment->type = 'video';
-                    $attachment->vk_id = $attachmentData->video->id;
+                    $attachment->vk_id = $attachmentData['video']['id'];
+                    if (isset($attachmentData['video']['image'][2]))
+                        $thumb = $attachmentData['video']['image'][2]['url'];
+                    else
+                        $thumb = $attachmentData['video']['image'][0]['url'];
                     $attachment->meta = [
-                        'thumb' => $attachmentData->video->photo_320,
-                        'duration' => $attachmentData->video->duration,
+                        'owner_id' => $attachmentData['video']['owner_id'],
+                        'thumb' => $thumb,
+                        'duration' => $attachmentData['video']['duration'],
                     ];
                     $attachment->save();
                     continue;
@@ -109,25 +111,25 @@ class VkWebhookController extends Controller
 
                 if ($type == 'audio') {
                     $attachment->type = 'audio';
-                    $attachment->vk_id = $attachmentData->audio->id;
+                    $attachment->vk_id = $attachmentData['audio']['id'];
                     $attachment->meta = [
-                        'owner_id' => $attachmentData->audio->owner_id,
-                        'artist' => $attachmentData->audio->artist,
-                        'title' => $attachmentData->audio->title,
+                        'owner_id' => $attachmentData['audio']['owner_id'],
+                        'artist' => $attachmentData['audio']['artist'],
+                        'title' => $attachmentData['audio']['title'],
                     ];
                     $attachment->save();
                     continue;
                 }
 
                 if ($type == 'poll') {
-                    if ($attachmentData->poll->owner_id != $postData->owner_id)
+                    if ($attachmentData['poll']['owner_id'] != $postData['owner_id'])
                         continue;
 
                     $attachment->type = 'poll';
-                    $attachment->vk_id = $attachmentData->poll->id;
+                    $attachment->vk_id = $attachmentData['poll']['id'];
                     $attachment->meta = [
-                        'artist' => $attachmentData->audio->artist,
-                        'title' => $attachmentData->audio->title,
+                        'artist' => $attachmentData['audio']['artist'],
+                        'title' => $attachmentData['audio']['title'],
                     ];
                     $attachment->save();
                     continue;
@@ -135,13 +137,13 @@ class VkWebhookController extends Controller
 
                 if ($type == 'doc') {
                     $attachment->type = 'doc';
-                    $attachment->vk_id = $attachmentData->doc->id;
+                    $attachment->vk_id = $attachmentData['doc']['id'];
                     $attachment->meta = [
-                        'owner_id' => $attachmentData->doc->owner_id,
-                        'title' => $attachmentData->doc->title,
-                        'size' => $attachmentData->doc->size,
-                        'type' => $attachmentData->doc->type,
-                        'url' => $attachmentData->doc->url,
+                        'owner_id' => $attachmentData['doc']['owner_id'],
+                        'title' => $attachmentData['doc']['title'],
+                        'size' => $attachmentData['doc']['size'],
+                        'type' => $attachmentData['doc']['type'],
+                        'url' => $attachmentData['doc']['url'],
                     ];
                     $attachment->save();
                     continue;
@@ -151,13 +153,15 @@ class VkWebhookController extends Controller
                     $attachment->type = 'link';
                     $attachment->vk_id = null;
                     $attachment->meta = [
-                        'title' => $attachmentData->link->title,
-                        'url' => $attachmentData->link->url,
+                        'title' => $attachmentData['link']['title'],
+                        'url' => $attachmentData['link']['url'],
                     ];
                     $attachment->save();
                     continue;
                 }
             }
+
+            return self::SUCCESS_TEXT;
         }
 
         return self::FALLBACK_TEXT;
