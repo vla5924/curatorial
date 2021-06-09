7d6ad729863ce1ca21d7ef0546bf915445a311ed
vla5924 <vla5924@gmail.com>
2021-06-09 21:12:10 +0300

    added hint for extra token

--- resources/lang/ru/settings.php
+++ resources/lang/ru/settings.php
@@ -20,4 +20,10 @@ return [
     'information_updated_successfully' => 'Информация в вашем профиле успешно обновлена.',
     'application_language' => 'Язык приложения',
     'settings_saved' => 'Настройки сохранены',
+    'extra_token_hint' => '<h5><i class="fas fa-exclamation-circle"></i> Подсказка</h5>
+<p>После перехода по ссылке "Авторизоваться и получить ключ доступа" и авторизации откроется пустая страница с адресом вида<br />
+<code>https://oauth.vk.com/blank.html#access_token=<b>24787&shy;9113e&shy;3bf1c&shy;43dde&shy;18ae9&shy;af06e&shy;ef247&shy;87911&shy;3e3bf&shy;1c43d&shy;de18a&shy;e9af0&shy;6eef</b>&expires_in=0&user_id=167790499&state=1</code>.<br />
+В этом случае ключом доступа будет являться строка <code>24787&shy;9113e&shy;3bf1c&shy;43dde&shy;18ae9&shy;af06e&shy;ef247&shy;87911&shy;3e3bf&shy;1c43d&shy;de18a&shy;e9af0&shy;6eef</code>.<br/>
+Аналогичным образом вам нужно найти ключ доступа в адресной строке открывшейся на вашем устройстве страницы.
+</p>',
 ];
