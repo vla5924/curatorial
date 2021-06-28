e62eed16788dcdd09ad0fbfe30a436eaecbb1fed
vla5924 <vla5924@gmail.com>
2021-06-29 02:29:59 +0300

    added group factory

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -9,6 +9,19 @@ class Group extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'vk_id',
+        'alias',
+        'vk_confirmation_token',
+        'timetable_url',
+    ];
+
     public function practices()
     {
         return $this->hasMany(Practice::class);
